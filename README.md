e2edcb12c265ecf779e2f269e04dc2c8ece82cc4
Kent-Khang <vinhkhang1969@gmail.com>
2021-02-19 15:41:09 +0700

    Kent

---
+++ README.md
@@ -0,0 +1,11 @@
+# Video Call App - WebRTC
+@Authors:
+To Vinh Khang, Bui Quang Khai, Ha Hai Dang
+
+Using:
+<br />• NodeJS
+<br />• ExpressJS
+<br />• SocketIO
+<br />• MongoDB
+
+Producted by KKQ Software Team
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-// Import libraries and app setup
+// ----------------
+// IMPORT LIBRARIES
+// ----------------
 const express = require("express");
-const { v4: v4UniqueId } = require("uuid");
+const { v4: v4 } = require("uuid");
 const http = require("http");
 const { ExpressPeerServer } = require("peer");
+
 const app = express();
 const port = process.env.PORT || 7000;
 const httpServer = http.Server(app);
@@ -10,12 +13,14 @@ const io = require("socket.io")(httpServer);
 const peerServer = ExpressPeerServer(httpServer, {
     debug: true,
 });
-
 app.set("view engine", "pug");
 app.use(express.static("public"));
 app.use("/peerjs", peerServer);
 
-// Server listen to the emitting messages from client to take on action
+// ------
+// SOCKET
+// ------
+// Server wait for the emitting action from client to on action
 io.on("connection", (socket) => {
     // When server has listened to the emitting message from user,
     // it will take the user to the specific room that the user is asking to entry
@@ -24,8 +29,7 @@ io.on("connection", (socket) => {
         socket.join(meetingRoomId);
         socket
             .to(meetingRoomId)
-            .broadcast.emit("new user has connected", userId);
-
+            .broadcast.emit("New user has connected", userId);
         // Server listen to the emitting message when client input their message
         // and emit back a message to allow client rendering new message on the web page
         socket.on("new message", (newMessage) => {
@@ -34,20 +38,62 @@ io.on("connection", (socket) => {
     });
 });
 
-// Entry endpoint
-// When enter into this endpoint it will also generate a unique room id and redirect you to that room
-app.get("/create", (req, res) => {
-    res.redirect(`/meetingroom/${v4UniqueId()}`);
+
+// ----
+// INIT
+// ----
+
+// HOME
+app.get("/", (req, res) => {
+    res.render("index");
 });
 
-// Meeting room endpoint
+// FORMS
+app.get("/signup", (req, res) => {
+    res.render("signup");
+});
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+
+// ---------
+// FUNCTIONS
+// ---------
+app.post("/signup", (req, res) => {
+    // ------------------
+    // HANDLE SIGNUP HERE
+    // ------------------
+
+    // IF SIGNUP SUCCESS:
+    //    --> res.redirect(`/`);
+    res.redirect(`/`);
+    // ------------------
+    // END HANDLE SIGNUP
+    // ------------------
+});
+app.post("/login", (req, res) => {
+    // ------------------
+    // HANDLE LOGIN HERE
+    // ------------------
+
+    // CHECK USER FROM DATABASE: 
+    //    IF USER VALID:
+    //        --> res.redirect(`/meetingroom/${v4()}`);
+    res.redirect(`/meetingroom/${v4()}`);
+    // ------------------
+    // END HANDLE LOGIN
+    // ------------------
+});
+// UNIQUE ROOM
 app.get("/meetingroom/:id", (req, res) => {
     res.render("meetingRoom", {
         meetingRoomId: req.params.id,
     });
 });
 
-// Server listen
+
+// LISTEN
 httpServer.listen(port, () => {
     console.log(`Server is running at port ${port}`);
 });
