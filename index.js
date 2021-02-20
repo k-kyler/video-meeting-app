5a1d96259f628a16b48debd23d93d1c9aa0f0a64
K-Kyler <khaiquang690@gmail.com>
2021-02-21 01:25:31 +0700

    fix conflicts

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-// ----------------
-// IMPORT LIBRARIES
-// ----------------
+// Import libraries and app setup
 const express = require("express");
 const { v4: v4UniqueId } = require("uuid");
 const http = require("http");
@@ -12,98 +10,182 @@ const io = require("socket.io")(httpServer);
 const peerServer = ExpressPeerServer(httpServer, {
     debug: true,
 });
+const bcrypt = require("bcrypt");
+const path = require("path");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const AccountModel = require("./models/account");
+const SESSION_NAME = "webrtc";
 
 app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
 app.use(express.static("public"));
 app.use("/peerjs", peerServer);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(
+    session({
+        name: SESSION_NAME,
+        secret: "secretcat",
+        resave: false,
+        saveUninitialized: true,
+        cookie: { maxAge: 1000 * 60 * 60 * 24 },
+    })
+);
 
-// ------
-// SOCKET
-// ------
-// Server wait for the emitting action from client to on action
+// Check if user not login
+const checkLogin = (req, res, next) => {
+    if (!req.session.username) {
+        res.redirect("/login");
+    } else {
+        next();
+    }
+};
+
+// Prevent user from accessing routes that don't need after logged in
+const preventWhenLogged = (req, res, next) => {
+    if (req.session.username) {
+        res.redirect("/");
+    } else {
+        next();
+    }
+};
+
+// Index endpoint
+app.get("/", (req, res) => {
+    if (req.session.username) {
+        res.render("index", {
+            username: req.session.username,
+        });
+    } else {
+        res.render("index");
+    }
+});
+
+// User log in endpoint
+app.get("/login", preventWhenLogged, (req, res) => {
+    res.render("login");
+});
+
+app.post("/login", preventWhenLogged, (req, res) => {
+    AccountModel.findOne({
+        username: req.body.username,
+    })
+        .then((data) => {
+            bcrypt
+                .compare(req.body.password, data.password)
+                .then(function (result) {
+                    if (result) {
+                        req.session.username = req.body.username;
+                        res.redirect("/");
+                    } else {
+                        res.render("login", {
+                            error: "Invalid email or password",
+                        });
+                    }
+                });
+        })
+        .catch((err) => {
+            res.render("login", {
+                error: "Invalid email or password",
+            });
+        });
+});
+
+// User sign up endpoint
+app.get("/signup", preventWhenLogged, (req, res) => {
+    res.render("signup");
+});
+
+app.post("/signup", preventWhenLogged, (req, res) => {
+    var username = req.body.username;
+    var password = req.body.password;
+    var confirmpassword = req.body.confirmpassword;
+    let encryptedPassword = "";
+
+    if (confirmpassword != password) {
+        res.render("signup", {
+            error: "Confirm password does not match",
+        });
+    } else {
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(password, salt, function (err, hash) {
+                encryptedPassword = hash;
+                AccountModel.findOne({
+                    username: username,
+                })
+                    .then((data) => {
+                        /* const passwordhash = bcrypt.hash(password, 10)
+                        const confirmpasswordhash = bcrypt.hash(confirmpassword, 10) */
+                        if (data) {
+                            res.render("signup", {
+                                error: "User has already existed",
+                            });
+                        } else {
+                            return AccountModel.create({
+                                username: username,
+                                password: encryptedPassword,
+                            });
+                        }
+                    })
+                    .catch((err) => {
+                        res.render("signup", {
+                            error: "User has already existed",
+                        });
+                    });
+            });
+        });
+    }
+});
+
+// User logout endpoint
+app.get("/logout", checkLogin, (req, res) => {
+    req.session.destroy((error) => {
+        if (error) {
+            res.redirect("/");
+        } else {
+            res.clearCookie(SESSION_NAME);
+            res.redirect("/");
+        }
+    });
+});
+
+// When enter into this endpoint it will also generate a unique room id and redirect you to that room
+app.get("/create", checkLogin, (req, res) => {
+    res.redirect(`/meetingroom/${v4UniqueId()}`);
+});
+
+// Meeting room endpoint
+app.get("/meetingroom/:id", checkLogin, (req, res) => {
+    if (req.session.username) {
+        res.render("meetingRoom", {
+            meetingRoomId: req.params.id,
+        });
+    } else {
+        res.redirect("/login");
+    }
+});
+
+// Server listen to the emitting messages from client to take on action
 io.on("connection", (socket) => {
     // When server has listened to the emitting message from user,
     // it will take the user to the specific room that the user is asking to entry
     // and emit all the users of that room to know that the new user has entried
-    socket.on("entry room", (meetingRoomId, userId) => {
+    socket.on("Entry room", (meetingRoomId, userId) => {
         socket.join(meetingRoomId);
         socket
             .to(meetingRoomId)
             .broadcast.emit("New user has connected", userId);
+
         // Server listen to the emitting message when client input their message
         // and emit back a message to allow client rendering new message on the web page
-        socket.on("new message", (newMessage) => {
-            io.to(meetingRoomId).emit("add new message", newMessage);
+        socket.on("New message", (newMessage) => {
+            io.to(meetingRoomId).emit("Add new message", newMessage);
         });
     });
 });
 
-
-// ----
-// INIT
-// ----
-
-// HOME
-app.get("/", (req, res) => {
-    res.render("index");
-});
-// RENDER
-app.get("/signup", (req, res) => {
-    res.render("signup");
-});
-app.get("/login", (req, res) => {
-    res.render("login");
-});
-app.get("/options", (req, res) => {
-    res.render("roomOption");
-});
-
-// ----------------
-// HANDLE FUNCTIONS
-// ----------------
-
-// SIGNUP & LOGIN
-app.post("/signup", (req, res) => {
-    // ------------------
-    // HANDLE SIGNUP HERE
-    // ------------------
-    // IF SIGNUP SUCCESS:
-    res.redirect(`/`);
-});
-app.post("/login", (req, res) => {
-    // ------------------
-    // HANDLE LOGIN HERE
-    // ------------------
-    // IF LOGIN SUCCESS:
-    res.redirect(`/options`);
-});
-
-// CREATE & JOIN ROOM
-app.post("/create", (req, res) => {
-    // ------------------
-    // HANDLE CREATE HERE
-    // ------------------
-    res.redirect(`/meetingroom/${v4UniqueId()}`);
-});
-app.post("/join", (req, res) => {
-    // ------------------
-    // HANDLE JOIN HERE
-    // ------------------
-    // CHECK ROOM STATUS (EXISTS, FULL)
-    // IF OK:
-    // --> REDIRECT TO ROOM WITH EXACT "roomID"
-    res.send(`Join Room Success`);
-});
-
-// UNIQUE ROOM
-app.get("/meetingroom/:id", (req, res) => {
-    res.render("meetingRoom", {
-        meetingRoomId: req.params.id,
-    });
-});
-
-
-// LISTEN
+// Server listen
 httpServer.listen(port, () => {
     console.log(`Server is running at port ${port}`);
 });
